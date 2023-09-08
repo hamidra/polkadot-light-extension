4761e34f74413af10ce1c1ff3729b6628888850b
Vadim <vadim.sav@gmail.com>
2023-09-08 12:47:45 +0300

    feat: HMR config (#48)
    * add proper HMR support
    
    * add permissions
    
    * moved manifest; fixed minor issues;
    
    * fixed build manifest issue
    
    * proper handling of manifest; clean up; fixed sourcemap warning;

--- apps/extension/vite.config.ts
+++ apps/extension/vite.config.ts
@@ -1,39 +1,49 @@
-// https://vitejs.dev/config/
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { crx } from "@crxjs/vite-plugin";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { resolve } from "path";
+import { manifest } from "./manifest";
 
 const src = resolve(__dirname, "src");
-const assetsDir = resolve(src, "assets");
 const outDir = resolve(__dirname, "dist");
 const publicDir = resolve(__dirname, "public");
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@src": src,
-      "@assets": assetsDir,
-    },
-  },
-  plugins: [react()],
-  publicDir,
-  build: {
-    outDir,
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        content: resolve(src, "content", "index.ts"),
-        background: resolve(src, "background", "index.ts"),
-        popup: resolve(src, "popup", "index.html"),
-        newtab: resolve(src, "newtab", "index.html"),
-        options: resolve(src, "options", "index.html"),
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      server: {
+        port: 8000,
+        hmr: {
+          host: "localhost",
+          port: 8000,
+        },
       },
-      output: {
-        entryFileNames: (chunk) => `src/${chunk.name}/index.js`,
+      plugins: [react(), crx({ manifest }), tsconfigPaths()],
+    };
+  } else {
+    return {
+      plugins: [react(), crx({ manifest }), tsconfigPaths()],
+      publicDir,
+      build: {
+        outDir,
+        sourcemap: true,
+        rollupOptions: {
+          input: {
+            content: resolve(src, "content", "index.ts"),
+            background: resolve(src, "background", "index.ts"),
+            popup: resolve(src, "popup", "index.html"),
+            newtab: resolve(src, "newtab", "index.html"),
+            options: resolve(src, "options", "index.html"),
+          },
+          output: {
+            entryFileNames: (chunk) => `src/${chunk.name}/index.js`,
+          },
+        },
       },
-    },
-  },
-  define: {
-    "import.meta.vitest": "undefined",
-  },
+      define: {
+        "import.meta.vitest": "undefined",
+      },
+    };
+  }
 });
